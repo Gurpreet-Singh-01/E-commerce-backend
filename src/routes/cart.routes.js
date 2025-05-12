8e25a4a2dbd6789bb6b0574df5e0d2c6915913f4
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-05-12 11:26:39 +0530

    Added add to cart route

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,9 +1,10 @@
+const { addToCart } = require('../controllers/cart.controller');
 const verifyJWT = require('../middlewares/auth.middleware');
 
 const router = require('express').Router();
 
 
 router.use(verifyJWT)
-
+router.post('/',addToCart)
 
 module.exports = router
