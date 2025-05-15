8c45e3a7c63d05eb1ac90c26320fc2fb44c2ff11
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-05-15 11:01:08 +0530

    Fixed Formatting

--- src/app.js
+++ src/app.js
@@ -1,38 +1,35 @@
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const CORS = require('cors')
-const errorMiddleware = require('./middlewares/error.middleware')
-const userRouter  = require('./routes/user.routes')
-const productRouter = require('./routes/product.routes')
-const categoryRouter = require('./routes/category.routes')
-const cartRouter = require('./routes/cart.routes')
-const orderRouter = require('./routes/order.routes')
-const adminRouter = require('./routes/admin.routes')
-const morgan = require('morgan')
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const CORS = require("cors");
+const errorMiddleware = require("./middlewares/error.middleware");
+const userRouter = require("./routes/user.routes");
+const productRouter = require("./routes/product.routes");
+const categoryRouter = require("./routes/category.routes");
+const cartRouter = require("./routes/cart.routes");
+const orderRouter = require("./routes/order.routes");
+const adminRouter = require("./routes/admin.routes");
+const morgan = require("morgan");
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(
-    CORS(
-        {
-            origin:process.env.CORS_ORIGIN,
-            credentials:true
-        }
-    )
-)
+  CORS({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-
-app.use(express.json({limit:'16kb'}))
-app.use(express.urlencoded({limit:'16kb'}))
-app.use(cookieParser())
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb" }));
+app.use(cookieParser());
+app.use(express.static("public"));
 
 // Routes
-app.use('/api/v1/user',userRouter)
-app.use('/api/v1/product',productRouter)
-app.use('/api/v1/category',categoryRouter)
-app.use('/api/v1/cart',cartRouter)
-app.use('/api/v1/order',orderRouter)
-app.use('/api/v1/admin', adminRouter)
-app.use(errorMiddleware)
-module.exports = {app}
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use(errorMiddleware);
+module.exports = { app };
